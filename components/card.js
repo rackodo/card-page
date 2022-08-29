5f88816ab8d26a37ac6c3e65b7d37e4917910ad4
Bash Elliott <42397332+rackodo@users.noreply.github.com>
2022-08-29 20:07:09 +1000

    finished design and polish, added hue slider. ready for push

--- components/card.js
+++ components/card.js
@@ -1,19 +1,43 @@
+import Head from 'next/head'
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/card.module.css'
+import PropTypes from 'prop-types'
 
-export default function Card( {title, image, alt, description} ) {
+import { createGlobalStyle } from 'styled-components'
+
+export default function Card( {title, image, description, hue, children} ) {
 	console.log(image)
+
+	const CustomPalette = createGlobalStyle`
+		html {
+			--bg: hsl(${hue}, 53%, 79%);
+			--secondary: hsl(${hue}, 54%, 65%);
+			--tertiary: hsl(${hue}, 44%, 48%)
+		}
+	`;
+
 	return(
 		<div className={styles.container}>
-			<Image
-			src={image}
-			width="200"
-			height="200"
-			alt={alt} />
-			<h1>{title}</h1>
-			<p>{description}</p>
+			<CustomPalette />
+			<div className={styles.imageWrapper}>
+				<Image
+				src={image}
+				width={250}
+				height={250}
+				alt={title}
+				className={styles.image} />
+			</div>
+			<h1 className={styles.title}>{title}</h1>
+			<p className={styles.description}>{description}</p>
+			{children}
 		</div>
 	)
 }
 
+Card.PropTypes = {
+	title: PropTypes.string.isRequired,
+	image: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	hue: PropTypes.number.isRequired
+};
--- components/cardlink.js
+++ components/cardlink.js
@@ -2,15 +2,25 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/cardlink.module.css'
 
-export default function CardLink( text, image, href ) {
+export default function CardLink({ text, image, href }) {
     console.log(image)
 	return(
-		<div className={styles.container}>
-			<Image
-			src={image}
-			width={64}
-			height={64}
-			alt="" />
+		<div className={styles.wrapper}>
+			<Link href={href}>
+				<a className={styles.container} target="_blank">
+					<div className={styles.imageWrapper}>
+						<Image
+						className={styles.image}
+						src={image}
+						width={48}
+						height={48}
+						alt={text} />
+					</div>
+					<div className={styles.label}>
+						{text}
+					</div>
+				</a>
+			</Link>
 		</div>
 	)
 }
